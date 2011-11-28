4b2be683c701faaacf834046669dcab7d52bff58
DEVARAJ <devu.dilip@gmail.com>
2011-11-28 19:00:02 +0530

    pages added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,26 @@
 class PagesController < ApplicationController
   def home
+    @title ='home'
   end
 
   def newcars
+    @title ='newcars'
   end
 
   def usedcars
+    @title ='usedcars'
   end
 
   def sellcar
+    @title ='sellcar'
   end
 
   def research
+    @title ='research'
   end
 
   def finance
+        @title ='finance'
   end
 
 end
